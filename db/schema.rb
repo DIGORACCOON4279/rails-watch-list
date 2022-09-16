65ddd3be0b2bc47ec76c390ba32b8d2666c13da4
diego marin <dilema4000@gmail.com>
2022-09-16 15:05:55 -0500

    Challenge Watch List3

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_175424) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_200107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
