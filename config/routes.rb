56ce34463ab64ef8b1b0b86a8b711b7d61155330
diego marin <dilema4000@gmail.com>
2022-09-16 14:13:02 -0500

    Challenge Watch list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Un usuario puede ver todas las listas
+  get "lists",                   to: "lists#index"
+  # Un usuario puede ver el nombre y el detalle de una lista específica
+  get "lists/:id",               to: "lists#show"
+  # Un usuario puede crear una nueva lista
+  get "lists/new",               to: "lists#new"
+  post "lists",                  to: "lists#create"
+
+  get "lists/:id/bookmarks/new", to: "lists#new"
+  post "lists/:id/bookmarks",    to: "lists#update"
+
+  #Un usuario puede borrar un bookmark de una lista. ¿Cómo podemos crear un link de delete?
+  delete "bookmarks/:id",         to: "lists#destroy"
 end
